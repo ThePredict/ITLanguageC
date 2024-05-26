91068a1bd3b484b85529f1684db9e9434e959eb9
ThePredict <dariodelatorre11111@gmail.com>
2024-05-26 14:29:37 +0000

    Added Var fullName and a prv string

--- main.cs
+++ main.cs
@@ -1,7 +1,12 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World! Dario is Here");
-  }
-}
+    public static void Main(string[] args) {
+      var fullName = ConcatName("Dario", "D Giavazzi");
+        Console.WriteLine("Hello World! " + fullName + " is Here");
+      }
+
+    private static string ConcatName(string firstName, string lastName) {
+        return firstName + " " + lastName;
+    }
+}  
