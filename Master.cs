189be9ef679f5eabbab1286826a5bec24e9cbf39
ThePredict <dariodelatorre11111@gmail.com>
2024-05-26 20:01:23 +0000

    Assignment 1.2 Unit Test 4

--- Master.cs
+++ Master.cs
@@ -3,14 +3,16 @@ class Program
 {
   public static void Main (string[] args) 
   {
-    string Lyrics1 = "Coming out of my cage";
-    string Lyrics2 = "And I've been doing just fine";
-    string Lyrics3 = "Gotta gotta be down";
-    string Lyrics4 = "Because I want it all";
+    string Lyrics1 = "Now this is a story all about how";
+    string Lyrics2 = "My life got flipped turned upside down";
+    string Lyrics3 = "And I'd like to take a minute";
+    string Lyrics4 = "Just sit right there";
+    string Lyrics5 = "I'll tell you how I became the prince of a town called Bel-Air";
     
     Console.WriteLine(Lyrics1);
     Console.WriteLine(Lyrics2);
     Console.WriteLine(Lyrics3);
     Console.WriteLine(Lyrics4);
+    Console.WriteLine(Lyrics5);
   }
 }
