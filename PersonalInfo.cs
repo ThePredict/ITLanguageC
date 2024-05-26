1f5cad0a843f5371b26d00736c2bd203ea48745d
ThePredict <dariodelatorre11111@gmail.com>
2024-05-26 19:09:10 +0000

    Unit Test #1 Test passed

---
+++ PersonalInfo.cs
@@ -0,0 +1,16 @@
+using System;
+class Program
+{
+  public static void Main (string[] args) 
+  {
+    var dateofbirth = "6/24/1992";
+    var WorkID = "000-000-0101";
+    var cellphone = "000-000-0189";
+    var personName = "Lucinda Potter";
+    
+    Console.WriteLine(personName);
+    Console.WriteLine(dateofbirth);
+    Console.WriteLine("work " + WorkID);
+    Console.WriteLine("cell " + cellphone);
+  }
+}
