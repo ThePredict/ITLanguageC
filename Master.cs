7c6b04ee135ba7ba8ca7060515eb9e1c7f8ad59e
ThePredict <dariodelatorre11111@gmail.com>
2024-06-03 01:20:35 +0000

    InchesToCentmeters

--- Master.cs
+++ Master.cs
@@ -1,18 +1,13 @@
 using System;
-class Program
-{
-  public static void Main (string[] args) 
+class Program {
+  public static void Main ()
   {
-    string Lyrics1 = "Now this is a story all about how";
-    string Lyrics2 = "My life got flipped turned upside down";
-    string Lyrics3 = "And I'd like to take a minute";
-    string Lyrics4 = "Just sit right there";
-    string Lyrics5 = "I'll tell you how I became the prince of a town called Bel-Air";
+   const double Cent_Per_Inch = 2.54;
+    double inches = 15994;
+    double centimeters = inches * Cent_Per_Inch;
+    Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);
     
-    Console.WriteLine(Lyrics1);
-    Console.WriteLine(Lyrics2);
-    Console.WriteLine(Lyrics3);
-    Console.WriteLine(Lyrics4);
-    Console.WriteLine(Lyrics5);
+    
   }
 }
+   
