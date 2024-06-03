3b27e2c3c4543ed17412f545be04e021cd988290
ThePredict <dariodelatorre11111@gmail.com>
2024-06-03 01:35:12 +0000

    InchesToCentimeterslnteractive

--- Master.cs
+++ Master.cs
@@ -4,8 +4,7 @@ class Program {
   {
    const double Cent_Per_Inch = 2.54;
     double inches = 15994;
-    double centimeters = inches * Cent_Per_Inch;
-    Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);
+    Console.WriteLine("{0} inches is {1} centimeters", inches, inches * Cent_Per_Inch);
     
     
   }
