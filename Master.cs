b561208a2d390311ec179044859a8b71f56fdeec
ThePredict <dariodelatorre11111@gmail.com>
2024-07-08 22:40:42 +0000

    Twitter

--- Master.cs
+++ Master.cs
@@ -1,19 +1,20 @@
 using System;
-class Program {
-  public static void Main ()
-  {
-   const double Base_Rate = 200;
-       const double Hour_Rate = 150;
-       const double Mile_Rate = 2;
-        double inputMiles = 3000;
-        double inputHours = 103;
-        double total = 0;
-    total = Base_Rate + (inputMiles * Mile_Rate) + (inputHours * Hour_Rate);
-    Console.WriteLine("{0} + {1} * {2} + {3} * {4}", Base_Rate, Hour_Rate, inputMiles, Mile_Rate, inputHours);
-    Console.WriteLine("The total is {0}", total);
-  }
-}
-    
-    
+class Program
+{
+    static void Main()
+    {
+        String string1 = " Platea dictumst quisque sagittis purus sit amet volutpat consequat mauris.";
+        int count = 0;
+        for(int i = 0; i < string1.Length;i++){
+            if(string1[i] !=' ') count++;
+        }
+        if (count > 140)
+        Console.WriteLine("Rejected");
+        else
+        Console.WriteLine("Posted");
+    }
+    }
+       
 
-   
+
+
