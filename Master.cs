e68ffe271ec0be91bbd3d10cbb14fe15b983eecf
ThePredict <dariodelatorre11111@gmail.com>
2024-07-11 23:42:37 +0000

    GreenvilleRevenue

--- Master.cs
+++ Master.cs
@@ -3,67 +3,53 @@ class Program
 {
     static void Main(string [] args)
     {
-       double sum = 0;
-        bool up = true;
-        bool down = true;
-        bool correct_temperature;
-        double temperature_range;
-        double[] temperature = new double[5];
-            for(int i = 0; i < 5; i++)
-        {
-            do
-            {
-                Console.Write("INPUT Temperature : ");
-                correct_temperature = double.TryParse(Console.ReadLine(), out temperature_range);
-                if (!correct_temperature || temperature_range < -30 || temperature_range > 130)
-                {
-                    Console.WriteLine("EXCEPTION Temperature " + (temperature_range) + " is invalid, Please enter a valid temperature between -30 and 130");
-                }
-            }
-            while(!correct_temperature || temperature_range < -30 || temperature_range > 130);
-            temperature[i] = temperature_range;
+        Console.WriteLine("************************************");
+        Console.WriteLine("*  The stars shine in Greenville.  *");
+        Console.WriteLine("************************************");
+        Console.WriteLine();
+        DisplayMenu();
         }
-        for(int i = 1; i < temperature.Length; i++)
+    static void DisplayMenu()
+    {
+        while(true)
         {
-            if(temperature[i] < temperature [i-1])
+            Console.WriteLine("Please Enter the following number below from the following menu: ");
+            Console.WriteLine("1. CALCULATE Greenville Revenue Year-Over-Year ");
+            Console.WriteLine("2. Exit ");
+            string option = Console.ReadLine();
+            switch(option)
             {
-                up = false;
-            }
-            else if (temperature[i] > temperature [i-1])
-            {
-                down = false;
+            case "1":
+                CalculateRevenue();
+                break;
+            case "2":
+                Console.WriteLine("Thank you for using the Greenville Revenue App, good-bye! ");
+                return;
+            default:
+                Console.WriteLine("Invalid option, please try again. ");
+                break;                                   
             }
         }
-        if(up)
-        {
-            Console.WriteLine("Getting warmer");
-        }
-        else if (down)
-        {
-            Console.WriteLine("Getting cooler");
-        }
-        else
-        {
-            Console.WriteLine("It's a mixed bag");
-        }
-        Console.Write("OUTPUT 5-day Temperature [");
-        for(int i = 0; i < temperature.Length; i++)
-        {
-            Console.Write(temperature[i]);
-            if(i < temperature.Length - 1)
-            {
-                Console.Write(",");
-            }   
-        }
-        Console.WriteLine("]");
-        foreach(double temp in temperature)
-        {
-            sum += temp;
-        }
-        double avgTemperature = sum / temperature.Length;
-        Console.WriteLine("OUTPUT Average Temperature is " + (avgTemperature) + " degrees");
     }
-}
+    static void CalculateRevenue()
+    {
+        Console.WriteLine("Please enter the number of contestants in previous year: ");
+        int previousYear = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter the number of contestants in current year: ");
+        int currentYear = int.Parse(Console.ReadLine());
+        int revenue = currentYear * 25;
+        bool isRevenueHigher = currentYear > previousYear;
 
+        Console.WriteLine($"Last year's competition had  {previousYear} contestants, and this year's has {currentYear} contestants.");
+        Console.WriteLine($"Revenue expected this year is {revenue}." );
+        Console.WriteLine($"It is {(isRevenueHigher ? "true" : "false")} that this year's competition is bigger than last year's. ");
+        Console.WriteLine();
+        Console.WriteLine("************************************");
+        Console.WriteLine("*  The stars shine in Greenville.  *");
+        Console.WriteLine("************************************");
+        Console.WriteLine();
+    }
+ }
+    
 
 
